a75603afe120ea8fcafb2488a9537e709db1f5da
Rajesh804 <rajesh804@gmail.com>
2016-12-20 14:14:08 +0530

    Added header for the script

--- setup_prerequisites.sh
+++ setup_prerequisites.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
+##############################################################
+#  Script     : setup_prerequisites
+#  Author     : Rajesh Reddy
+#  Date       : 19/12/2016
+#  Last Edited: 20/12/2016, Rajesh Reddy
+#  Description: The script to setup ansible and terraform
+##############################################################
+
+GREEN=`tput setaf 2`
+RED=`tput setaf 1`
 
 #To install and setup Ansible
-#sudo apt-add-repository ppa:ansible/ansible
-#sudo apt-get update -y
-#sudo apt-get install ansible -y
+sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get update -y
+sudo apt-get install ansible -y
 
 if [ -d "terraform/bin" ] ; then 
 	echo "bin dir already exists"
@@ -12,7 +22,7 @@ else
 fi
 
 #To install terraform
-echo *********Setting up terraform*********
+echo "*********Setting up terraform*********"
 if [ -e terraform_0.8.1_linux_amd64.zip ]; then
 	echo "Terraform ZIP already exists in $PWD. Checking for binnary"
 	if [ -e "terraform/bin/terraform" ]; then
@@ -25,10 +35,9 @@ else
 	wget https://releases.hashicorp.com/terraform/0.8.1/terraform_0.8.1_linux_amd64.zip
 	unzip terraform_0.8.1_linux_amd64.zip -d terraform/bin
 fi
-echo " "
 
 #To install teraform-inventory
-echo *********Setting up terraform-inventory*********
+echo "*********Setting up terraform-inventory*********"
 
 if [ -e terraform-inventory_v0.6.1_linux_amd64.zip ]; then
 	echo "Terraform Inventor ZIP already exists in $PWD. Checking for binnary"
@@ -42,16 +51,6 @@ else
 	wget https://github.com/adammck/terraform-inventory/releases/download/v0.6.1/terraform-inventory_v0.6.1_linux_amd64.zip
 	unzip terraform-inventory_v0.6.1_linux_amd64.zip -d terraform/bin
 fi
-
-echo " "
-
-#Setup up ansbile dependencies
-echo "Checking for required docker modules"
-ansible-doc -l | grep -e docker_container -e docker_image -e docker_image_facts -e docker_login -e docker_service
-
-echo " "
-GREEN=`tput setaf 2`
-RED=`tput setaf 1`
 
 if [ $? == 0 ];then
 	echo $GREEN"Prerequisties setup completed successfully"
